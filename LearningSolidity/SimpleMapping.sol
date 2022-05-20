40fa22746e11f03f8734be51875a701f255c6708
"bhartisharma01" <"sharma.bharti2608@gmail.com">
2022-05-20 22:51:19 +0530

    initial commit

---
+++ LearningSolidity/SimpleMapping.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.13;
+
+contract SimpleMapping{
+    mapping(uint => bool) public myMapping;
+    mapping(address => bool) public myAddressMapping;
+
+    function setValue(uint _index) public{
+        myMapping[_index] = true;
+    }
+
+    function setMyAddressToTrue() public{
+        myAddressMapping[msg.sender] = true;
+    }
+}
